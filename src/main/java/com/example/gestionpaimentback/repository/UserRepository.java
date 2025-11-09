53826334d38a26ca2ab48f18c8e55d67a8e03e36
hammamiyasmine <hammamiyasmine2890@gmail.com>
2025-11-09 15:05:21 +0000

    Mise à jour backend : partie admin

--- src/main/java/com/example/gestionpaimentback/repository/UserRepository.java
+++ src/main/java/com/example/gestionpaimentback/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.example.gestionpaimentback.repository;
 
+import com.example.gestionpaimentback.entity.Role;
 import com.example.gestionpaimentback.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r = :role")
+    List<User> findUsersByRole(Role role);
+
+    boolean existsById(Long id);
 }
